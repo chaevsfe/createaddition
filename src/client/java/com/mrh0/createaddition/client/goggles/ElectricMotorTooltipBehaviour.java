6e2eae3e899d666a27bd0a9f5847024cada11e6a
chaevsfe <pie@duck.com>
2026-09-23 01:39:18 -0400

    Show the Electric Motor energy usage at its set speed while it is stopped

--- src/client/java/com/mrh0/createaddition/client/goggles/ElectricMotorTooltipBehaviour.java
+++ src/client/java/com/mrh0/createaddition/client/goggles/ElectricMotorTooltipBehaviour.java
@@ -8,6 +8,7 @@ import com.mrh0.createaddition.client.CALang;
 import com.mrh0.createaddition.util.Util;
 import com.zurrtum.create.client.foundation.blockEntity.behaviour.tooltip.GeneratingKineticTooltipBehaviour;
 import com.zurrtum.create.client.foundation.utility.CreateLang;
+import com.zurrtum.create.foundation.blockEntity.behaviour.scrollValue.ServerScrollValueBehaviour;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
@@ -21,11 +22,13 @@ public class ElectricMotorTooltipBehaviour extends GeneratingKineticTooltipBehav
 	@Override
 	public boolean addToGoggleTooltip(List<Component> tooltip, boolean isPlayerSneaking) {
 		super.addToGoggleTooltip(tooltip, isPlayerSneaking);
+		ServerScrollValueBehaviour speed = blockEntity.getBehaviour(ServerScrollValueBehaviour.TYPE);
+		int rpm = speed == null ? 0 : speed.getValue();
 		CALang.builder()
 			.add(Component.translatable(CreateAddition.MODID + ".tooltip.energy.consumption").withStyle(ChatFormatting.GRAY))
 			.forGoggles(tooltip);
 		CALang.builder()
-			.add(Component.literal(" " + Util.format(ElectricMotorBlockEntity.getEnergyConsumptionRate(blockEntity.getGeneratedSpeed())) + "⚡/t ")
+			.add(Component.literal(" " + Util.format(ElectricMotorBlockEntity.getEnergyConsumptionRate(rpm)) + "⚡/t ")
 				.withStyle(ChatFormatting.AQUA)
 				.append(CreateLang.translateDirect("gui.goggles.at_current_speed").withStyle(ChatFormatting.DARK_GRAY)))
 			.forGoggles(tooltip);
